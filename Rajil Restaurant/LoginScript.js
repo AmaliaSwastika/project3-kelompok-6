bf56f84b177cdc6373f768e2826b9dcaa33c68a5
AmaliaSwastika <102946499+AmaliaSwastika@users.noreply.github.com>
2022-06-23 00:39:18 +0700

    Add files via upload

---
+++ Rajil Restaurant/LoginScript.js
@@ -0,0 +1,16 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="admin"&& password=="user")
+{
+    alert("Login Succesfully!");
+    location.assign("Reservation.html")
+    return false;
+}
+else
+{
+    alert("Login Failed!");
+}
+
+}
